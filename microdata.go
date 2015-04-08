e57a12407af4a84c0c40a813257d64191c81955b
Lars Wiegman <lars@namsral.com>
2015-04-08 15:51:37 +0200

    Removed reduntanted code and split complex code to easier to digest functions to improve code simplicity and readability.

--- microdata.go
+++ microdata.go
@@ -9,8 +9,8 @@
 
 	Usage:
 
-	Pass a reader, baseURL and contentType to the Parse function.
-		data, err := microdata.Parse(reader, baseURL, contentType)
+	Pass a reader, content-type and a base URL to the ParseHTML function.
+		data, err := microdata.ParseHTML(reader, contentType, baseURL)
 		items := data.Items
 
 	Pass an URL to the ParseURL function.
@@ -85,7 +85,6 @@ type parser struct {
 
 // parse returns the microdata from the parser's node tree.
 func (p *parser) parse() (*Microdata, error) {
-
 	toplevelNodes := []*html.Node{}
 
 	walkNodes(p.tree, func(n *html.Node) {
@@ -101,107 +100,39 @@ func (p *parser) parse() (*Microdata, error) {
 
 	for _, node := range toplevelNodes {
 		item := NewItem()
-		p.data.Items = append(p.data.Items, item)
-		if s, ok := getAttr("itemtype", node); ok {
-			for _, itemtype := range strings.Split(s, " ") {
-				if len(itemtype) > 0 {
-					item.addType(itemtype)
-				}
-			}
-
-			if s, ok := getAttr("itemid", node); ok {
-				if u, err := p.baseURL.Parse(s); err == nil {
-					item.Id = u.String()
-				}
-			}
-		}
-
-		if s, ok := getAttr("itemref", node); ok {
-			for _, itemref := range strings.Split(s, " ") {
-				if len(itemref) > 0 {
-					if n, ok := p.identifiedNodes[itemref]; ok {
-						p.readItem(item, n)
-					}
-				}
-			}
-		}
-
-		for c := node.FirstChild; c != nil; c = c.NextSibling {
-			p.readItem(item, c)
-		}
+		p.data.addItem(item)
+		p.readItem(item, node)
 	}
+
 	return p.data, nil
 }
 
-// readItem parses the given node and populates the given item with the result.
+// readItem traverses the given node tree, applying relevant attributes to the
+// given item.
 func (p *parser) readItem(item *Item, node *html.Node) {
-	if itemprops, ok := getAttr("itemprop", node); ok {
-		if _, ok := getAttr("itemscope", node); ok {
-			subItem := NewItem()
+	itemprops, hasProp := getAttr("itemprop", node)
+	_, hasScope := getAttr("itemscope", node)
 
-			if itemrefs, ok := getAttr("itemref", node); ok {
-				for _, itemref := range strings.Split(itemrefs, " ") {
-					if len(itemref) > 0 {
-						if n, ok := p.identifiedNodes[itemref]; ok {
-							p.readItem(subItem, n)
-						}
-					}
-				}
+	switch {
+	case hasScope && !hasProp:
+		p.readAttr(item, node)
+	case hasScope && hasProp:
+		subItem := NewItem()
+		p.readAttr(subItem, node)
+		for _, propName := range strings.Split(itemprops, " ") {
+			if len(propName) > 0 {
+				item.addItem(propName, subItem)
 			}
-
-			for c := node.FirstChild; c != nil; c = c.NextSibling {
-				p.readItem(subItem, c)
-			}
-
+		}
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			p.readItem(subItem, c)
+		}
+		return
+	case !hasScope && hasProp:
+		if s := p.getValue(node); len(s) > 0 {
 			for _, propName := range strings.Split(itemprops, " ") {
 				if len(propName) > 0 {
-					item.addItem(propName, subItem)
-				}
-			}
-			return
-		} else {
-			var propValue string
-
-			switch node.DataAtom {
-			case atom.Meta:
-				if value, ok := getAttr("content", node); ok {
-					propValue = value
-				}
-			case atom.Audio, atom.Embed, atom.Iframe, atom.Img, atom.Source, atom.Track, atom.Video:
-				if value, ok := getAttr("src", node); ok {
-					if u, err := p.baseURL.Parse(value); err == nil {
-						propValue = u.String()
-					}
-				}
-			case atom.A, atom.Area, atom.Link:
-				if value, ok := getAttr("href", node); ok {
-					if u, err := p.baseURL.Parse(value); err == nil {
-						propValue = u.String()
-					}
-				}
-			case atom.Data, atom.Meter:
-				if value, ok := getAttr("value", node); ok {
-					propValue = value
-				}
-			case atom.Time:
-				if value, ok := getAttr("datetime", node); ok {
-					propValue = value
-				}
-			default:
-				var buf bytes.Buffer
-				walkNodes(node, func(n *html.Node) {
-					if n.Type == html.TextNode {
-						buf.WriteString(n.Data)
-					}
-				})
-				propValue = buf.String()
-			}
-
-			if len(propValue) > 0 {
-				for _, propName := range strings.Split(itemprops, " ") {
-					if len(propName) > 0 {
-						item.addString(propName, propValue)
-					}
+					item.addString(propName, s)
 				}
 			}
 		}
@@ -210,6 +141,75 @@ func (p *parser) readItem(item *Item, node *html.Node) {
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
 		p.readItem(item, c)
 	}
+}
+
+// readAttr applies relevant attributes from the given node to the given item.
+func (p *parser) readAttr(item *Item, node *html.Node) {
+	if s, ok := getAttr("itemtype", node); ok {
+		for _, itemtype := range strings.Split(s, " ") {
+			if len(itemtype) > 0 {
+				item.addType(itemtype)
+			}
+		}
+
+		if s, ok := getAttr("itemid", node); ok {
+			if u, err := p.baseURL.Parse(s); err == nil {
+				item.Id = u.String()
+			}
+		}
+	}
+
+	if s, ok := getAttr("itemref", node); ok {
+		for _, itemref := range strings.Split(s, " ") {
+			if len(itemref) > 0 {
+				if n, ok := p.identifiedNodes[itemref]; ok {
+					p.readItem(item, n)
+				}
+			}
+		}
+	}
+}
+
+// getValue returns the value of the property, value pair in the given node.
+func (p *parser) getValue(node *html.Node) string {
+	var propValue string
+
+	switch node.DataAtom {
+	case atom.Meta:
+		if value, ok := getAttr("content", node); ok {
+			propValue = value
+		}
+	case atom.Audio, atom.Embed, atom.Iframe, atom.Img, atom.Source, atom.Track, atom.Video:
+		if value, ok := getAttr("src", node); ok {
+			if u, err := p.baseURL.Parse(value); err == nil {
+				propValue = u.String()
+			}
+		}
+	case atom.A, atom.Area, atom.Link:
+		if value, ok := getAttr("href", node); ok {
+			if u, err := p.baseURL.Parse(value); err == nil {
+				propValue = u.String()
+			}
+		}
+	case atom.Data, atom.Meter:
+		if value, ok := getAttr("value", node); ok {
+			propValue = value
+		}
+	case atom.Time:
+		if value, ok := getAttr("datetime", node); ok {
+			propValue = value
+		}
+	default:
+		var buf bytes.Buffer
+		walkNodes(node, func(n *html.Node) {
+			if n.Type == html.TextNode {
+				buf.WriteString(n.Data)
+			}
+		})
+		propValue = buf.String()
+	}
+
+	return propValue
 }
 
 // newParser returns a parser that converts the content of r to UTF-8 based on the content type of r.
